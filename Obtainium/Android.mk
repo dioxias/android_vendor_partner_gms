078ccf2be1cdc11e93d65966f81ec9fbb35a7e37
skye <skye@dioxias.net>
2024-03-10 21:51:54 -0400

    initial

---
+++ Obtainium/Android.mk
@@ -0,0 +1,20 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := privapp-permissions-dev.imranr.obtainium.xml
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT_ETC)/permissions
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := Obtainium
+LOCAL_SRC_FILES := Obtainium.apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_REQUIRED_MODULES := privapp-permissions-dev.imranr.obtainium.xml
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_PRODUCT_MODULE := true
+include $(BUILD_PREBUILT)
+
